8a747822cba26c46b42244ac7fee40f686e4ecd5
Arjan Landman <landman.arjan@gmail.com>
2011-11-08 14:01:31 +0100

    Removed overrides

--- app/overrides/add_content_for_head_to_show_product.rb
+++
@@ -1,5 +0,0 @@
-    Deface::Override.new(:virtual_path => "products/show",
-                         :name => "converted_cart_form_594755007",
-                         :insert_before => "[data-hook='cart_form'], #cart-form[data-hook]",
-                         :partial => "products/content_for_head",
-                         :disabled => false)
--- app/overrides/add_customizations_to_cart_form.rb
+++
@@ -1,5 +0,0 @@
-    Deface::Override.new(:virtual_path => "products/_cart_form",
-                         :name => "converted_product_price_290656527",
-                         :insert_after => "[data-hook='product_price'], #product_price[data-hook]",
-                         :partial => "products/customizations",
-                         :disabled => false)
