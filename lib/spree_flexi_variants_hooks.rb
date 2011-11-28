fc7970159f2878b00c61c445cb0d6c4b6dd2ee7f
Arjan Landman <landman.arjan@gmail.com>
2011-11-28 07:53:59 +0100

    changed ad_hoc_option hook

--- lib/spree_flexi_variants_hooks.rb
+++ lib/spree_flexi_variants_hooks.rb
@@ -57,9 +57,9 @@ class SpreeFlexiVariantsHooks < Spree::ThemeSupport::HookListener
     # we are old-school spree (0.60.* and below)
     insert_after :admin_product_sub_tabs, 'admin/products/additional_product_sub_menu'
     insert_after :admin_product_tabs, 'admin/products/additional_product_tabs'
-    insert_after :product_price, 'products/ad_hoc_option_types'
     insert_after :product_price, 'products/customizations'
     insert_after :product_price, 'products/pricing'
+    insert_after :product_price, 'products/ad_hoc_option_types'
 
     replace :order_details_line_item_row, 'shared/order_details_line_item_row'
     replace :cart_item_description, 'orders/cart_item_description'
